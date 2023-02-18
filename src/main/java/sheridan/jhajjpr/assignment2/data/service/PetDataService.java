3cf8b1eb859ac6f718c3c092901d77f4cf64b478
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-02-18 14:30:31 -0500

    Finished code for PetDataService.java

---
+++ src/main/java/sheridan/jhajjpr/assignment2/data/service/PetDataService.java
@@ -0,0 +1,20 @@
+package sheridan.jhajjpr.assignment2.data.service;
+
+import sheridan.jhajjpr.assignment2.model.PetsForm;
+import java.util.List;
+
+
+public interface PetDataService {
+
+    void insertPetForm(PetsForm form);
+
+    List<PetsForm> getAllPetsForm();
+
+    void deleteAllPetForms();
+
+    void deletePetForm(int id);
+
+    PetsForm getPetForm(int id);
+
+    void updatePetForm(PetsForm form);
+}
